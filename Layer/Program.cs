dcade49a18dd5f3e80926faf9cf9ced91acf5f3d
6U-U9 <604391451@qq.com>
2022-08-08 18:53:56 +0800

    fuck 4 fin

--- Layer/Program.cs
+++ Layer/Program.cs
@@ -135,8 +135,8 @@ void iterate(NodeSet set)
         }
         layerList = nextLayerList;
     }
-    //layerList = set.mergeLayer(layerList, set.nodeSet.Values.ToHashSet().Count / 4);
-    layerList = set.mergeLayerWithoutZero(layerList, set.nodeSet.Values.ToHashSet().Count / 2);
+    //layerList = set.mergeLayer(layerList, set.nodeSet.Values.ToHashSet().Count / 2);
+    layerList = set.mergeLayerv2WithoutZero(layerList);
     //layerList = set.mergeLayerv2Max(layerList);
     set.genAlgorithmLayer(layerList);
     set.algorithm = "迭代分层";
@@ -152,7 +152,7 @@ void improved(NodeSet set)
     var topoList = set.generateTopoList(set.nodeSet.Values.ToHashSet());
     //var testIn = set.generateLayerIn(topoList);
     var layerList = set.generateNewLayers(topoList);
-    layerList = set.mergeLayerWithoutZero(layerList, set.nodeSet.Values.ToHashSet().Count / 2);
+    layerList = set.mergeLayerv2WithoutZero(layerList);
     set.genAlgorithmLayer(layerList);
     set.algorithm = "改进依赖";
     set.layerList = layerList;
@@ -165,7 +165,7 @@ void improved(NodeSet set)
 void MaxDepth(NodeSet set)
 {
     var layerList = set.generateLayerBasedMaxDepth();
-    layerList = set.mergeLayerWithoutZero(layerList, set.nodeSet.Values.ToHashSet().Count / 2);
+    layerList = set.mergeLayerv2WithoutZero(layerList);
     
 
     set.relayerCirclePackage(layerList);
